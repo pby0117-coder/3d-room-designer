d6a29b2fbb9d9fcff12abda4c3fb19c4f6f00f36
pby0117-coder <pby0117@gmail.com>
2025-10-19 15:47:54 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RoomDesignerApp from "./RoomDesignerApp";
-import "./index.css"; // 스타일 시트 (선택사항)
+//import "./index.css"; // 스타일 시트 (선택사항)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
